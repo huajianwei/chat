2e5c8964458f4c017a934820edd1a31c18157a53
panyantao <hwamei7@163.com>
2015-09-17 14:24:53 +0800

    add the sdk of easemob sdk.

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by panyantao on 15/9/17.
+//  Copyright (c) 2015年 hwamei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by panyantao on 15/9/17.
+//  Copyright (c) 2015年 hwamei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
